22afb3a66de4c671292e0a106ab3629dca224fd5
Bartłomiej Gargula <bartgargula@gmail.com>
2020-05-08 09:55:31 +0200

    JS changes

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const name = 'Bartek';
 const age = 28;
 
 console.log(`Cześć! Jestem ${name} i mam ${age} lat. To jest moja strona z notatkami ze szkolenia WTF: Co ten frontend.`)
+
+const heading = document.querySelector('.header__title--js');
+
+heading.innerHTML = 'Biografia'
