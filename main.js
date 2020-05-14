d130097d5e6873ceee24ddddb57cfc8fd991258b
Bartłomiej Gargula <bartgargula@gmail.com>
2020-05-14 13:41:20 +0200

    JS greet added

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const name = 'Bartek';
 const age = 28;
 
-console.log(`Cześć! Jestem ${name} i mam ${age} lat. To jest moja strona z notatkami ze szkolenia WTF: Co ten frontend.`)
-
+function helloWorld (){
+    console.log('witaj świecie');
+}
 const heading = document.querySelector('.header__title--js');
 
-heading.innerHTML = 'Biografia'
+heading.innerHTML = `Cześć! Jestem ${name} i mam ${age} lat. To jest moja strona z notatkami ze szkolenia WTF: Co ten frontend.`
+
+
+function greet(age, name) {
+    console.log(
+        `Cześć! Jestem ${name} i mam ${age} lat. To jest moja strona z notatkami ze szkolenia WTF: Co ten frontend.`)
+    }
+
+greet (32, 'Krzysiek')
+
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content
+}
+
+createContent('.week-summary__subheading--jss', 'Witam wszystkich')
